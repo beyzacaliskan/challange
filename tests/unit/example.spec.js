c0c7309662cf057b1949bf6436d6c6edebc8cd64
Beyza <caliskanbeyza@gmail.com>
2021-08-20 17:57:15 +0300

    simple test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('Header.vue', () => {
+  it('renders header', () => {
+    const wrapper = shallowMount(Header)
+    expect(wrapper.find('#nav').exists())
   })
 })
